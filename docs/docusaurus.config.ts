863dbfa7498800450d438d56093919a3e5218894
Ciro <ciro.napolitano87@gmail.com>
2024-12-23 14:01:38 +0100

    bugfix docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/cironapo/cn-multiselect/tree/main/docs/',
         },
         theme: {
           customCss: './src/css/custom.css',
