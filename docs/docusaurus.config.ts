46af027f168f31e5f544fa6cec94060b186f19b7
Ciro <ciro.napolitano87@gmail.com>
2024-12-23 12:54:46 +0100

    mod docusaurus

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,15 +10,15 @@ const config: Config = {
   //favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://cironapo.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cn-multiselect/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'cironapo', // Usually your GitHub org/user name.
+  projectName: 'cn-multiselect', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
