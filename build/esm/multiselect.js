2f3d8b725e415bf651e709e51431b8c162de7f8f
cironapo <cironapo@users.noreply.github.com>
2024-12-23 12:39:44 +0000

    deploy: b7c7c6ea85f61d813de8512b3aa7e1cb4e4b6fea

---
+++ build/esm/multiselect.js
@@ -0,0 +1,20 @@
+import { p as promiseResolve, b as bootstrapLazy } from './index-1da239bf.js';
+export { s as setNonce } from './index-1da239bf.js';
+import { g as globalScripts } from './app-globals-0f993ce5.js';
+
+/*
+ Stencil Client Patch Browser v4.23.0 | MIT Licensed | https://stenciljs.com
+ */
+var patchBrowser = () => {
+  const importMeta = import.meta.url;
+  const opts = {};
+  if (importMeta !== "") {
+    opts.resourcesUrl = new URL(".", importMeta).href;
+  }
+  return promiseResolve(opts);
+};
+
+patchBrowser().then(async (options) => {
+  await globalScripts();
+  return bootstrapLazy([["cn-multiselect",[[65,"cn-multiselect",{"name":[1],"selectAll":[1028,"select-all"],"preserveOrder":[1028,"preserve-order"],"selectAllText":[1,"select-all-text"],"deselectAllText":[1,"deselect-all-text"],"disabledItems":[1,"disabled-items"],"options":[1025],"selected":[1025],"title":[1],"placeholderSearch":[1,"placeholder-search"],"disabled":[4],"multiple":[4],"search":[1028],"popup":[1028],"maxSelectedItems":[2,"max-selected-items"],"popupTitle":[1,"popup-title"],"_disabled_items":[32],"_options":[32],"_selected":[32],"open":[32],"searchKey":[32],"selectAllItems":[64],"deselectAllItems":[64]},[[8,"click","clickOutsideHandler"]],{"options":["optionsWatcher"],"disabledItems":["disabledItemsWatcher"],"selected":["selectedWatcher"]}]]]], options);
+});
