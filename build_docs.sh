0e0ab4b99052f40709f44edae131790349b0fff8
Ciro <ciro.napolitano87@gmail.com>
2025-01-02 22:32:48 +0100

    mod docs

--- build_docs.sh
+++ build_docs.sh
@@ -3,4 +3,4 @@ npx stencil build --docs
 cp ./src/components/cn-multiselect/readme.md ./docs/docs/documentation/docs.md
 
 echo '---\nsidebar_position: 1\n---' | cat - ./docs/docs/documentation/docs.md | sed "s|# cn-multiselect|# Documentation|g" > ./temp
-mv ./temp ./docs/docs/docs.md
+mv ./temp ./docs/docs/documentation/docs.md
